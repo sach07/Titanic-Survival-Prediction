f3492581662b413c9b0042804e62270df2576e50
Sachin Gupta <sachinsg873@gmail.com>
2018-09-30 15:18:09 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting survival in titanic disaster',
+    author='Sachin gupta[D[D[D[D[D[3~[3~[C[C[C[C[C',
+    license='MIT',
+)
